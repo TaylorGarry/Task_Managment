ee6d45d5e612fd05786abb5a5553737eadaa8fa5
TaylorGarry <TaylorGarry798@gmail.com>
2025-12-11 23:01:54 +0530

    Fix for jwt expired issue

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../Modals/User.modal.js";
 
-const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // export const signup = async (req, res) => {
 //   try {
